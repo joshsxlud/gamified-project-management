165bd43de14d9f866759c8e614f0b4f846299388
Ronen Begelfor <rbegelfor@rrc.ca>
2026-03-02 23:37:05 -0600

    feat: Completed group/s repository.

---
+++ src/apis/groupRepo.ts
@@ -0,0 +1,52 @@
+import type { Group } from "../types/groupType";
+
+
+// Get all groups attached to an organization
+export function fetchGroups(): Group[] {
+    return groupData;
+}
+
+// Creates a new group
+export function createGroup(group: Group): Group {
+    const newGroup: Group = {
+    ...group,
+
+    id: group.id ?? crypto.randomUUID(),
+    };
+    groupData.push(newGroup);
+    return newGroup;
+}
+
+// Get a group by its id
+export function getGroupById(groupId: string): Group {
+    const foundGroup = groupData.find((g) => g.id === groupId);
+
+    if (!foundGroup) {
+        throw new Error(`Failed to fetch group with id ${groupId}`);
+    }
+
+    return foundGroup;
+}
+
+// Updates a group, ensures to get the group's id first
+export async function updateGroup(group: Group): Promise<void> {
+    const foundGroupIndex = groupData.findIndex((g) => g.id === group.id);
+
+    if (foundGroupIndex === -1) {
+            throw new Error(`Failed to update group with id ${group.id}`);
+        }
+    
+    groupData[foundGroupIndex] = group;
+    return groupData[foundGroupIndex];
+}
+
+// Deletes a group using its id
+export function deleteGroup(groupId: string): void {
+    const foundGroup = groupData.find((g) => g.id === groupId);
+
+    if (foundGroup === -1) {
+            throw new Error(`Failed to delete group with id ${groupId}`);
+        }
+    
+    groupData.splice(foundGroup, 1);
+}
