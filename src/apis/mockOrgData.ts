5ebe38baa59d5072b64c5421d162f8062bbc8619
ejuayang-rrc <ejuayang@rrc.ca>
2026-03-02 19:30:10 -0600

    Created mock data for Organizations.

---
+++ src/apis/mockOrgData.ts
@@ -0,0 +1,19 @@
+import type { Organization } from "../types/organizationType";
+
+export const orgData: Organization[] = [
+  {
+    "id": 1,
+    "name": "Doofenshmirtz Evil Incorporated",
+    "groups": [1, 2, 3]
+  },
+  {
+    "id": 3,
+    "name": "Umbrella Corporation",
+    "groups": [4, 5, 6]
+  },
+  {
+    "id": 4,
+    "name": "My House",
+    "groups": [7, 8, 9]
+  }
+]
