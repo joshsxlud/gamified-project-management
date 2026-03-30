8531c51c4a37f3eedfef84dd9d1c8ead90b29f4f
joshsxlud <jsalud2@academic.rrc.ca>
2026-03-30 14:52:05 -0500

    feat: basic controllers + seed data.

---
+++ apps/backend/prisma/migrations/20260330192626_add_default_instead_of_nullable/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - Made the column `description` on table `Task` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "Task" ALTER COLUMN "description" SET NOT NULL,
+ALTER COLUMN "description" SET DEFAULT '';
