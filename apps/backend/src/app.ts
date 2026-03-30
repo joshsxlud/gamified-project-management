c219be85040615f04fc024381eaa8d1791579dcb
Ronen Begelfor <rbegelfor@rrc.ca>
2026-03-30 14:53:45 -0500

    feat/fix: Moved cors config into a back-end config folder. Updated cors config.

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,33 +1,16 @@
 import express, {Express} from "express";
 import cors from "cors";
 
+import { corsOptions }  from "../config/cors";
+
 // initialize express application
 const app: Express = express();
 
-//RB This is the temporary way for the allowed origin as we are currently running locally.
-const allowedOrigin = 'http://localhost:5173';
+// cors middleware
+app.use(cors(corsOptions));
 
 // allow express to parse json
 app.use(express.json());
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin) {
-            return callback(null, true);
-        }
-        
-        if (origin === allowedOrigin)
-        {
-            callback(null, true);
-        } else {
-            // Just for now, in the future this would actually be important to add.
-            return callback(new Error('This domain is prohibited from accessing this backend!'))
-        }
-    },
-    methods: [
-        'GET', 'POST', 'PUT', 'DELETE'
-    ]
-
-}));
 
 // listen for requests on root and send simple text response
 app.get("/",  (_req, res) => {
