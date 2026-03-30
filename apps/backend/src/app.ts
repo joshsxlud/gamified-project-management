446c828989ddc5b25981cd54d13ac2085905148e
Ronen Begelfor <rbegelfor@rrc.ca>
2026-03-30 14:09:00 -0500

    feat: installed new packages, cors and cors types.

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,4 +1,5 @@
 import express, {Express} from "express";
+import cors from "cors";
 
 // initialize express application
 const app: Express = express();
@@ -6,6 +7,7 @@ const app: Express = express();
 
 // allow express to parse json
 app.use(express.json());
+app.use(cors());
 
 // listen for requests on root and send simple text response
 app.get("/",  (_req, res) => {
