6d1ffc30eb68d3004a42644bb69c2d02843b02e8
joshsxlud <jsalud2@academic.rrc.ca>
2026-03-31 01:15:20 -0500

    fix: re added other pages.

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,29 +1,29 @@
-// import Footer from "./components/common/footer/Footer";
-// import './App.css'
-// import Header from "./components/common/header/Header";
-// import TaskDashboard from "./components/pages/TaskDashboard";
-// import DepartmentDashboard from "./components/pages/DepartmentDashboard";
-// import { Routes, Route } from "react-router-dom";
-
-// function App() {
-
-//   return (
-//     <>
-//       <Header />
-//       <Routes>
-//         <Route path="/" element={<TaskDashboard />} />
-//         <Route path="/task-dashboard" element={<TaskDashboard />} />
-//         <Route path="/department-dashboard" element={<DepartmentDashboard />} />
-//       </Routes>
-//       <Footer />
-//     </>
-//   )
-// }
-
-// export default App
-
+import Footer from "./components/common/footer/Footer";
+import './App.css'
+import Header from "./components/common/header/Header";
 import TaskDashboard from "./components/pages/TaskDashboard";
+import DepartmentDashboard from "./components/pages/DepartmentDashboard";
+import { Routes, Route } from "react-router-dom";
+
 function App() {
-  return <TaskDashboard />;
+
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<TaskDashboard />} />
+        <Route path="/task-dashboard" element={<TaskDashboard />} />
+        <Route path="/department-dashboard" element={<DepartmentDashboard />} />
+      </Routes>
+      <Footer />
+    </>
+  )
 }
-export default App;
+
+export default App
+
+// import TaskDashboard from "./components/pages/TaskDashboard";
+// function App() {
+//   return <TaskDashboard />;
+// }
+// export default App;
