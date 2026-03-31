d287486a85a315c7dff40b2e0ad0b5487f41ae42
Ronen Begelfor <rbegelfor@rrc.ca>
2026-03-30 21:10:12 -0500

    feat: created department service.

---
+++ apps/backend/src/api/v1/services/departmentService.ts
@@ -0,0 +1,83 @@
+import type { Group } from "../../../../generated/prisma/client";
+import { prisma } from "../../../../prisma/client";
+import { DepartmentCreateModel } from "../models/departments/departmentCreateModel";
+import { DepartmentUpdateModel } from "../models/departments/departmentUpdateModel";
+
+/**
+ * Service to get all departments.
+ * 
+ * @returns A list of all departments.
+ */
+export const getAllDepartments = async (): Promise<Group[]> => {
+
+    try {
+        const departments = await prisma.group.findMany();
+        return structuredClone(departments);
+    } catch (error) {
+        throw new Error("Failed to retrieve all departments.");
+    }
+}
+
+/**
+ * Service to get a department by id.
+ * 
+ * @param id - The id of a department.
+ * @returns Group - A department object.
+ */
+export const getDepartmentById = async (id: number): Promise<Group> => {
+
+    try {
+        const department: Group | null = await prisma.group.findUnique({
+            where: {
+                id: id,
+            }
+        });
+
+        if (!department){
+            throw new Error(`Could not get department with id ${"id"}.`)
+        }
+
+        return structuredClone(department);
+    } catch (error) {
+
+        throw new Error(`Department with id ${id} could not be retrieved.`);
+    }
+}
+
+export const createDepartment = async (groupData: DepartmentCreateModel): Promise<Group> => {
+    try {
+        const newDepartment = await prisma.group.create({
+            data: {
+                name: groupData.name,
+                numberOfUsers: groupData.numberOfUsers,
+                organizationId: groupData.organizationId,
+            }
+        });
+        return structuredClone(newDepartment);
+    } catch (error) {
+        throw new Error(`Department could not be created.`);
+    }
+}
+
+export const updateDepartment = async (id: number, updateData: DepartmentUpdateModel): Promise<Group> => {
+    try {
+        const updateDepartment = await prisma.group.update({
+            where: { id },
+            data: updateData
+        });
+
+        return structuredClone(updateDepartment);
+    } catch (error) {
+        throw new Error(`Department with id ${id} could not be updated.`);
+    }
+}
+
+export const deleteDepartment = async (id: number): Promise<void> => {
+    try {
+        await prisma.group.delete({
+            where: {id}
+        });
+    } catch (error) {
+        throw new Error(`Department with id ${id} could not be deleted.`)
+    }
+}
